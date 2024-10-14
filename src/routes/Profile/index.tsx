bf9ec30aa4af759b103e046073e3fc98fb74b152
enzobueno11 <127800256+enzobueno11@users.noreply.github.com>
2024-10-14 18:25:17 -0300

    [kan-12] addUserProfile

--- src/routes/Profile/index.tsx
+++ src/routes/Profile/index.tsx
@@ -1,9 +1,26 @@
+import React from 'react';
+import { useStore } from '../../store';
+
 const Profile = () => {
+  const { username, profilePic, posts } = useStore();
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <div className="profile-header">
+        <img src={profilePic} alt="Profile" className="profile-pic" />
+        <h2>{username}</h2>
+        <button>Edit Profile</button>
+      </div>
+
+      <div className="post-grid">
+        {posts.map((post, index) => (
+          <div key={index} className="post">
+            <img src={post} alt={`post-${index}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Profile;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,23 @@
 import { create } from 'zustand'
 
 type Store = {
-  posts: string[]
+  posts: string[],
+  username: string,
+  profilePic: string,
+  friendUsername: string,
+  friendProfilePic: string,
+  friendPosts: string[],
+  isFriend: boolean
 }
 
 export const useStore = create<Store>((set) => ({
   posts: [],
-}))
+  username: 'defaultUsername',
+  profilePic: '/defaultProfilePic.png',
+  friendUsername: 'FriendUsername',
+  friendProfilePic: '/defaultFriendPic.png',
+  friendPosts: [],
+  isFriend: false,
+}));
+
+
